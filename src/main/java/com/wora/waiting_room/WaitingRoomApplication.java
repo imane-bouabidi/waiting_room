0309e542a486bd0a91d8ee4adfa3ed285a321875
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-13 17:36:58 +0100

    project

---
+++ src/main/java/com/wora/waiting_room/WaitingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.wora.waiting_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wora/waiting_room/WaitingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.waiting_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
