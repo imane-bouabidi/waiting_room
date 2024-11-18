83d70279667f012787a652ae91fb8047b3f17b17
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-18 14:41:21 +0100

    waiting room service

--- src/main/java/com/wora/waiting_room/services/servicesIntr/VisitService.java
+++ src/main/java/com/wora/waiting_room/services/servicesIntr/VisitService.java
@@ -3,15 +3,16 @@ package com.wora.waiting_room.services.servicesIntr;
 import com.wora.waiting_room.dtos.VisitDTO.VisitCreateDTO;
 import com.wora.waiting_room.dtos.VisitDTO.VisitDTO;
 import com.wora.waiting_room.dtos.VisitDTO.VisitUpdateDTO;
+import com.wora.waiting_room.entities.embedded.VisitEmbeddedId;
 import com.wora.waiting_room.services.GenericService;
 
 import java.util.List;
 
-public interface VisitService extends GenericService<VisitCreateDTO, VisitUpdateDTO, VisitDTO, Long> {
+public interface VisitService extends GenericService<VisitCreateDTO, VisitUpdateDTO, VisitDTO, VisitEmbeddedId> {
     VisitDTO save(VisitCreateDTO createDto);
-    VisitDTO findById(Long id);
-    VisitDTO update(VisitUpdateDTO updateDto, Long id);
+    VisitDTO findById(VisitEmbeddedId id);
+    VisitDTO update(VisitUpdateDTO updateDto, VisitEmbeddedId id);
     List<VisitDTO> findAll();
-    void delete(Long id);
+    void delete(VisitEmbeddedId id);
 }
 
---
+++ src/main/java/com/wora/waiting_room/services/servicesIntr/WaitingRoomService.java
@@ -0,0 +1,16 @@
+package com.wora.waiting_room.services.servicesIntr;
+
+import com.wora.waiting_room.dtos.WaitingRoomDTO.WaitingRoomCreateDTO;
+import com.wora.waiting_room.dtos.WaitingRoomDTO.WaitingRoomDTO;
+import com.wora.waiting_room.dtos.WaitingRoomDTO.WaitingRoomUpdateDTO;
+import com.wora.waiting_room.services.GenericService;
+
+import java.util.List;
+
+public interface WaitingRoomService extends GenericService<WaitingRoomCreateDTO, WaitingRoomUpdateDTO, WaitingRoomDTO, Long> {
+    WaitingRoomDTO save(WaitingRoomCreateDTO createDto);
+    WaitingRoomDTO findById(Long id);
+    WaitingRoomDTO update(WaitingRoomUpdateDTO updateDto, Long id);
+    List<WaitingRoomDTO> findAll();
+    void delete(Long id);
+}
