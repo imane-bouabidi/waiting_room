35bd7be14b24e97bdda717acf4c0f985516d8d0c
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-18 14:35:31 +0100

    vistit service

--- src/main/java/com/wora/waiting_room/services/servicesIntr/VisitService.java
+++ src/main/java/com/wora/waiting_room/services/servicesIntr/VisitService.java
@@ -1,4 +1,17 @@
 package com.wora.waiting_room.services.servicesIntr;
 
-public class VisitService {
+import com.wora.waiting_room.dtos.VisitDTO.VisitCreateDTO;
+import com.wora.waiting_room.dtos.VisitDTO.VisitDTO;
+import com.wora.waiting_room.dtos.VisitDTO.VisitUpdateDTO;
+import com.wora.waiting_room.services.GenericService;
+
+import java.util.List;
+
+public interface VisitService extends GenericService<VisitCreateDTO, VisitUpdateDTO, VisitDTO, Long> {
+    VisitDTO save(VisitCreateDTO createDto);
+    VisitDTO findById(Long id);
+    VisitDTO update(VisitUpdateDTO updateDto, Long id);
+    List<VisitDTO> findAll();
+    void delete(Long id);
 }
+
