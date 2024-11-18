2ddf9573b0e1d49451b75e09a7bcbdf4d6dd0d80
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-18 14:22:29 +0100

    repositories

--- src/main/java/com/wora/waiting_room/repositories/VisitRepo.java
+++ src/main/java/com/wora/waiting_room/repositories/VisitRepo.java
@@ -1,8 +1,9 @@
 package com.wora.waiting_room.repositories;
 
 import com.wora.waiting_room.entities.Visit;
+import com.wora.waiting_room.entities.embedded.VisitEmbeddedId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisitRepo extends JpaRepository<Visit, Long> {
+public interface VisitRepo extends JpaRepository<Visit, VisitEmbeddedId> {
 
 }
